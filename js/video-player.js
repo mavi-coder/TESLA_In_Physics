24dff93620e3da90f1b8e4409be4ca9ce149906c
wael-abd-ALWHAB <139469407+wael-abd-ALWHAB@users.noreply.github.com>
2023-09-23 20:42:26 +0300

    Update video-player.js

--- js/video-player.js
+++ js/video-player.js
@@ -1,13 +1,13 @@
-let videoList = document.querySelectorAll('.video-list-container .list');
+let videoList = document.querySelectorAll('.list-content .list');
 
 videoList.forEach(vid =>{
    vid.onclick = () =>{
       videoList.forEach(remove =>{remove.classList.remove('active')});
       vid.classList.add('active');
-      let src = vid.querySelector('.list-video').src;
+      let src = vid.querySelector('.list-item').src;
       let title = vid.querySelector('.list-title').innerHTML;
-      document.querySelector('.main-video-container .main-video').src = src;
-      document.querySelector('.main-video-container .main-video').play();
-      document.querySelector('.main-video-container .main-vid-title').innerHTML = title;
+      document.querySelector('.main-content .main-item').src = src;
+      document.querySelector('.main-content .main-item').play();
+      document.querySelector('.main-content .main-vid-title').innerHTML = title;
    };
 });
