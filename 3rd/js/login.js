76b237f332f4d569c476726a2e0972b12c93dddf
wael-abd-ALWHAB <139469407+wael-abd-ALWHAB@users.noreply.github.com>
2024-06-03 00:58:01 +0300

    Update login.js

--- 3rd/js/login.js
+++ 3rd/js/login.js
@@ -1,16 +1,22 @@
- function login() {
-            const email = document.getElementById('email').value;
-            const password = document.getElementById('password').value;
+const accounts = [{ email: "example@gmail.com", password: "1234" },
+                  { email: "example2@gmail.com", password: "4321"},
+                  { email: "example3@gmail.com", password: "1324"} 
+];
 
-            if (email === 'example@gmail.com' && password === '1234') {
-                document.getElementById('login-form').style.display = 'none';
-                document.getElementById('content').style.display = 'block';
-                document.body.style.display = 'block';
-            } else {
-                alert('البريد الإلكتروني أو كلمة المرور غير صحيحة');
-            }
-        }
+function login() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-        document.addEventListener('DOMContentLoaded', (event) => {
-            document.body.style.display = 'block';
-        });
+    const user = accounts.find(account => account.email === email && account.password === password);
+    if (user) {
+        document.getElementById('login-form').style.display = 'none';
+        document.getElementById('content').style.display = 'block';
+        document.body.style.display = 'block';
+    } else {
+        alert('البريد الإلكتروني أو كلمة المرور غير صحيحة');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.body.style.display = 'block';
+});
