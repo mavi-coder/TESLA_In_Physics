c5cf9a35c1d1f6562de6b389851e3e4ff2a83108
wael-abd-ALWHAB <139469407+wael-abd-ALWHAB@users.noreply.github.com>
2024-06-15 00:36:03 +0300

    Update login.js

--- 3rd/js/login.js
+++ 3rd/js/login.js
@@ -1,7 +1,8 @@
 const accounts = [{ phone: "01066258336", password: "f1g6n" }, 
                   { phone: "01559831235", password: "o2n3" },
                   { phone: "01125286165", password: "n5t9" },
-                  { phone: "01012835186", password: "o7y5" }
+                  { phone: "01012835186", password: "o7y5" },
+                  { phone: "01019943079", password: "b4v6" }
                   
                 
 ];
