6a76a663a4fc7d2649f8280da4231b864598eefd
wael-abd-ALWHAB <139469407+wael-abd-ALWHAB@users.noreply.github.com>
2024-09-12 14:40:08 +0300

    Update login.js

--- 3rd/js/login.js
+++ 3rd/js/login.js
@@ -4,13 +4,13 @@ window.onload = function() {
     // إذا كان المستخدم قد سجل دخوله
     if (localStorage.getItem('userEmail')) {
         // إذا كان المستخدم في صفحة تسجيل الدخول، وجهه إلى الصفحة الشخصية
-        if (currentPage === 'login.html') {
-            window.location.href = 'home.html';
+        if (currentPage === 'https://wael-abd-alwhab.github.io/TESLA_In_Physics/3rd/login.html') {
+            window.location.href = 'https://wael-abd-alwhab.github.io/TESLA_In_Physics/3rd/home.html';
         }
     } else {
         // إذا لم يكن قد سجل الدخول ولم يكن بالفعل في صفحة تسجيل الدخول، وجهه إلى صفحة تسجيل الدخول
-        if (currentPage !== 'login.html') {
-            window.location.href = 'login.html';
+        if (currentPage !== 'https://wael-abd-alwhab.github.io/TESLA_In_Physics/3rd/login.html') {
+            window.location.href = 'https://wael-abd-alwhab.github.io/TESLA_In_Physics/3rd/login.html';
         }
     }
 };
